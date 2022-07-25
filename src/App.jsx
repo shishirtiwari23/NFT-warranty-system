1f7af22e08c1fa6532ba30b5efa6f1443b6b9e01
Shishir Tiwari <shishir456tiwari@gmail.com>
2022-07-25 23:13:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Hello</header>
+    </div>
+  );
+}
+
+export default App;

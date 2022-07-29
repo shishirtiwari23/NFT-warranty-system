022336e208580f058dc2c17e4c5fccce21fffab5
Shishir Tiwari <shishir456tiwari@gmail.com>
2022-07-29 16:26:14 +0530

    changes commit

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,20 @@
 import styles from "./Cards.module.scss";
 import { icons } from "../../assets";
 import { Button } from "../";
+import { TextInputField } from "../";
+import { useState } from "react";
 
 const { verifiedIcon } = icons;
 
 export const NFTCard = ({ NFT, ...remaining }) => {
+  const [isTransferModalOpen, setIsTransferModalOpen] = useState(false);
+  const [receiversWalletAddress, setReceiversWalletAddress] = useState("");
+
+  async function transferHandler(e) {
+    e.preventDefault();
+    setIsTransferModalOpen(true);
+  }
+
   const { date, id, name, warrantyDuration } = NFT;
   return (
     <div {...remaining} className={styles.NFTCard}>
@@ -22,9 +32,48 @@ export const NFTCard = ({ NFT, ...remaining }) => {
       </p>
       <div className={styles.buttons}>
         <Button>Raise An Issue</Button>
-        <Button variant="secondary">Transfer OwnerShip</Button>
+        <Button onClick={transferHandler} variant="secondary">
+          Transfer OwnerShip
+        </Button>
       </div>
+      {isTransferModalOpen && (
+        <TransferModal
+          receiversWalletAddress={receiversWalletAddress}
+          setIsTransferModalOpen={setIsTransferModalOpen}
+          setReceiversWalletAddress={setReceiversWalletAddress}
+          transferHandler={transferHandler}
+        />
+      )}
     </div>
+  );
+};
+
+export const TransferModal = ({
+  receiversWalletAddress,
+  setReceiversWalletAddress,
+  transferHandler,
+  setIsTransferModalOpen,
+}) => {
+  return (
+    <form onSubmit={transferHandler} className={styles.transferModalContainer}>
+      <div className={styles.modal}>
+        <TextInputField
+          placeholder="0x00000000000000000000000000000"
+          value={receiversWalletAddress}
+          onChange={(e) => setReceiversWalletAddress(e.target.value)}
+          label={"Receiver's Wallet Address"}
+        />
+        <div className={styles.buttons}>
+          <Button>Transfer</Button>
+          <Button
+            onClick={() => setIsTransferModalOpen(false)}
+            style={{ background: "red", border: "2px solid red" }}
+          >
+            Cancel
+          </Button>
+        </div>
+      </div>
+    </form>
   );
 };
 
